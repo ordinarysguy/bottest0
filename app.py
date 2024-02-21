439c63d8667476665961f4c563d2668c041c042b
ordinarysguy <79636707+ordinarysguy@users.noreply.github.com>
2024-02-21 21:30:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(有效))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage('有效'))
     try:
         GPT_answer = GPT_response(msg)
         print(GPT_answer)
