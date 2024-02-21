c05202711a969eb57ab2a45677629657e736e66b
ordinarysguy <79636707+ordinarysguy@users.noreply.github.com>
2024-02-22 00:14:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def handle_message(event):
     try:
         msg = event.message.text
         massage=ImageSendMessage(
-                original_content_url = "https://www.google.com/search?q=%E8%B2%93%E5%92%AA&tbm=isch&ved=2ahUKEwiUlPfH4byEAxXFXvUHHR_eCMMQ2-cCegQIABAA&oq=%E8%B2%93%E5%92%AA&gs_lp=EgNpbWciBuiyk-WSqjIFEAAYgAQyCBAAGIAEGLEDMggQABiABBixAzIIEAAYgAQYsQMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARI4BxQrhNYuBpwAHgAkAEAmAE7oAGfAqoBATe4AQPIAQD4AQGKAgtnd3Mtd2l6LWltZ8ICBBAjGCfCAgcQABiABBgYiAYB&sclient=img&ei=mxjWZdTaKsW91e8Pn7yjmAw&bih=632&biw=1396&prmd=nivsbmtz&rlz=1C1VDKB_zh-TWTW954TW954#imgrc=UJOVbdwXLXE1mM",
-            preview_image_url = "https://www.google.com/search?q=%E8%B2%93%E5%92%AA&tbm=isch&ved=2ahUKEwiUlPfH4byEAxXFXvUHHR_eCMMQ2-cCegQIABAA&oq=%E8%B2%93%E5%92%AA&gs_lp=EgNpbWciBuiyk-WSqjIFEAAYgAQyCBAAGIAEGLEDMggQABiABBixAzIIEAAYgAQYsQMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARI4BxQrhNYuBpwAHgAkAEAmAE7oAGfAqoBATe4AQPIAQD4AQGKAgtnd3Mtd2l6LWltZ8ICBBAjGCfCAgcQABiABBgYiAYB&sclient=img&ei=mxjWZdTaKsW91e8Pn7yjmAw&bih=632&biw=1396&prmd=nivsbmtz&rlz=1C1VDKB_zh-TWTW954TW954#imgrc=UJOVbdwXLXE1mM")
+                original_content_url = "https://fluv.com/blog/wp-content/uploads/sites/2/2023/08/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88-9.png",
+            preview_image_url = "https://fluv.com/blog/wp-content/uploads/sites/2/2023/08/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88-9.png")
         line_bot_api.reply_message(event.reply_token, massage)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
     except:
