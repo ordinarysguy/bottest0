21e34c1726166ce588c0ed98b9355955c6fb1dbe
ordinarysguy <79636707+ordinarysguy@users.noreply.github.com>
2024-02-22 00:03:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def handle_message(event):
     try:
         msg = event.message.text
         massage=ImageSendMessage(
-                original_content_url = "https://photos.google.com/photo/AF1QipPU_VYwhrcb8OM70InBNtep6kLRwhC8lWvVWbLr",
-            preview_image_url = "https://photos.google.com/photo/AF1QipPU_VYwhrcb8OM70InBNtep6kLRwhC8lWvVWbLr")
+                original_content_url = "https://www.google.com/search?q=%E8%B2%93%E5%92%AA&tbm=isch&ved=2ahUKEwiUlPfH4byEAxXFXvUHHR_eCMMQ2-cCegQIABAA&oq=%E8%B2%93%E5%92%AA&gs_lp=EgNpbWciBuiyk-WSqjIFEAAYgAQyCBAAGIAEGLEDMggQABiABBixAzIIEAAYgAQYsQMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARI4BxQrhNYuBpwAHgAkAEAmAE7oAGfAqoBATe4AQPIAQD4AQGKAgtnd3Mtd2l6LWltZ8ICBBAjGCfCAgcQABiABBgYiAYB&sclient=img&ei=mxjWZdTaKsW91e8Pn7yjmAw&bih=632&biw=1396&prmd=nivsbmtz&rlz=1C1VDKB_zh-TWTW954TW954#imgrc=UJOVbdwXLXE1mM",
+            preview_image_url = "https://www.google.com/search?q=%E8%B2%93%E5%92%AA&tbm=isch&ved=2ahUKEwiUlPfH4byEAxXFXvUHHR_eCMMQ2-cCegQIABAA&oq=%E8%B2%93%E5%92%AA&gs_lp=EgNpbWciBuiyk-WSqjIFEAAYgAQyCBAAGIAEGLEDMggQABiABBixAzIIEAAYgAQYsQMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARI4BxQrhNYuBpwAHgAkAEAmAE7oAGfAqoBATe4AQPIAQD4AQGKAgtnd3Mtd2l6LWltZ8ICBBAjGCfCAgcQABiABBgYiAYB&sclient=img&ei=mxjWZdTaKsW91e8Pn7yjmAw&bih=632&biw=1396&prmd=nivsbmtz&rlz=1C1VDKB_zh-TWTW954TW954#imgrc=UJOVbdwXLXE1mM")
         line_bot_api.reply_message(event.reply_token, massage)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
     except:
