1dcf1c1814b3c0b76f7f2f4e593e8e026cc72483
ordinarysguy <79636707+ordinarysguy@users.noreply.github.com>
2024-02-27 02:14:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-line_bot_api.push_message('U14064b6b005dcd289f44ef6a2c106a36',TextSendMessage('部屬完成') )
+#line_bot_api.push_message('U14064b6b005dcd289f44ef6a2c106a36',TextSendMessage('部屬完成') )
 
 
 def GPT_response(text):
@@ -162,7 +162,7 @@ def handle_message(event):
     try:
         msg = event.message.text
         id = event.source.user_id
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(id) )
+        lline_bot_api.push_message(id,TextSendMessage('可以打字了') )
         '''
         json_data=json.loads(msg)
         type=json_data['events'][0]['message']['type']
